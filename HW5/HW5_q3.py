c1fd3332026579a85c3f4a32c715a26c29c69336
coledavis99 <coledavis6499@gmail.com>
2021-11-05 15:10:17 -0500

    I think laplace is done for everything

--- HW5/HW5_q3.py
+++ HW5/HW5_q3.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import copy
 from sklearn.svm import SVC
 from pomegranate import *
 
@@ -9,7 +10,7 @@ df = pd.read_csv('contact-lenses (copy).csv')
 
 
 '''
-For each column in the dataset, calculate the ratio of each distinct vals occurrance, grouped 
+For each col in the dataset, calculate the ratio of each distinct vals occurrance, grouped 
 by the distinct value of the decision attribute. These are the raw conditional probabilities
 which will soon be altered and laplace'd.
 '''
@@ -25,12 +26,12 @@ colList = []
 distinctNonDecValList = []
 distinctDecValList = []
 
-for col in list(df[:-1]):                               #Loop over all but the last column of the dataframe
+for col in list(df)[:-1]:                               #Loop over all but the last column of the dataframe
     for distinctNonDecVal in df[col].unique():          #Loop over each unique non-decision attribute value
         for distinctDecVal in df[decAttr].unique():     #Loop over each unique decision attribute value
             conditional_prob_table[col+distinctNonDecVal+distinctDecVal] = 0
 
-            #Store these, we'll need them to iterate over the conditional probability list
+            #Store these, we'll need them to iterate over the conditional probability list and know which node we're talking about
             if col not in colList:
                 colList.append(col)
             if distinctDecVal not in distinctDecValList:
@@ -45,54 +46,51 @@ dflen = len(colList)
 for idx, row in df.iterrows():
     colctr = 0
     for cell in row:
-        print(row.index[colctr])            #colname
-        print(cell)                         #cell value
-        print(row[dflen-1])                 #Decision attribute value for the row
-        print()
+        if(row.index[colctr] != decAttr):
+            # print(row.index[colctr])            #colname
+            # print(cell)                         #cell value
+            # print(row[dflen])                   #Decision attribute value for the row
+            # print()
 
-        conditional_prob_table[str(row.index[colctr]) + str(cell) + str(row[dflen-1])] += 1
-        colctr += 1
+            conditional_prob_table[str(row.index[colctr]) + str(cell) + str(row[dflen])] += 1
+            colctr += 1
     print()
 
 
-#Now calculate the conditional probability for each combination.
-for col in list(df[:-1]):                               #Loop over all but the last column of the dataframe
+#Store a copy of the count of each combination of colname, cell value, and decision attribute.
+combination_snapshot = copy.deepcopy(conditional_prob_table)
+
+colctr = 0
+#Now calculate the conditional probability for each non-dec combination.
+for col in list(df)[:-1]:                               #Loop over all but the decision attr column of the dataframe
     for distinctNonDecVal in df[col].unique():          #Loop over each unique non-decision attribute value
-        for distinctDecVal in df[decAttr].unique():     #Loop over each unique decision attribute value
-            conditional_prob_table[col+distinctNonDecVal+distinctDecVal] = conditional_prob_table[col+distinctNonDecVal+distinctDecVal] / len(df)
+        for distinctDecVal in df[decAttr].unique():     #Loop over each unique decision attribute value                                   # num of times a col has this non-dec val occur
+            conditional_prob_table[col+distinctNonDecVal+distinctDecVal] = conditional_prob_table[col+distinctNonDecVal+distinctDecVal] / df[col].value_counts().at[distinctNonDecVal]
+        colctr += 1
+
+#Now calculate the conditional probability for each dec combination.
+for val in distinctDecValList:
+    conditional_prob_table[decAttr+val] = df[decAttr].value_counts().at[val]/len(df)
 
 
-#Debugging Loop
+
+
+'''##########################################################################################
+#Apply Laplace smoothing to all values within the conditional_prob_table
+##########################################################################################'''
+
+#Laplace Loop for dec attributes
+LMBDA = 1
+for val in distinctDecValList:
+    conditional_prob_table[decAttr+val] = (df[decAttr].value_counts().at[val] + LMBDA) / (len(df) + (len(distinctDecValList) * LMBDA))
+
+
+#Laplace Loop for non-dec attributes
+for col in list(df)[:-1]:                              
+    for distinctNonDecVal in df[col].unique():          
+        for distinctDecVal in df[decAttr].unique():
+            conditional_prob_table[col+distinctNonDecVal+distinctDecVal] = (combination_snapshot[col+distinctNonDecVal+distinctDecVal] + LMBDA) / ((df[decAttr].value_counts().at[distinctDecVal]) + (len(list(df[col].unique())) * LMBDA))
+
+#Debugging Loop 2
 for key in conditional_prob_table:
-    print("Key: " + key + " " + "Value: " + str(conditional_prob_table[key]))
-
-
-'''
-#Generate a Pandas "Series" object that contains the count of each distinct value per column.
-value_counts = {}
-for col in df1:
-    value_counts[col] = df1[col].value_counts()
-
-#Loop over the Series object and extract the values we want. Store in a dict of dicts.
-conditional_probs = dict()
-labelctr = 0
-for key in value_counts:
-    conditional_probs[key] = dict()
-    print(key)
-    labelctr = 0
-    for val in value_counts[key]:
-        conditional_probs[key][value_counts[key].axes[0][labelctr]] = val
-        print(value_counts[key].axes[0][labelctr])
-        labelctr += 1
-    print()
-
-
-#Calculate the conditional probability for each distinct value
-total_rows = len(df1)
-for key1 in conditional_probs:
-    print("Col: " + key1)
-    for key2 in conditional_probs[key1]:
-        conditional_probs[key1][key2] = conditional_probs[key1][key2] / total_rows
-        print("Distinct Val: " + key2 + " " + str(conditional_probs[key1][key2]))
-    print()
-'''
+    print("Key: " + key + ": \t" + str(conditional_prob_table[key]))
